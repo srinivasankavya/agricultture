3a06d190022d3a9a5a26cbd55d06c88412d71326
anvishapathak <91515358+anvishapathak@users.noreply.github.com>
2023-11-02 17:38:34 +0530

    GUI using react

---
+++ body.js
@@ -0,0 +1,18 @@
+import "../../index.css";
+import "./body.css";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="background-image grid place-items-center my-6">
+        <div className="">
+          <p className="text-4xl font-medium text-center text-green-600 max-w-md mb-4">
+            Crop and Fertilizer recommendation
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
